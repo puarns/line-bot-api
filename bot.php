b3d1420df0267572cf10dc9e24138637d0730354
puarns <puarns@gmail.com>
2017-03-06 19:32:44 +0700

    Create bot.php

---
+++ bot.php
@@ -0,0 +1,21 @@
+<?php
+include ('line-bot-api/php/line-bot.php');
+
+$channelSecret = 'dca964b72850d1a98104e1ddfd8a34e7';
+$access_token  = 'eYrUwuYVBvWMCWaMOgVVpJ+340T6phdQcs/McASQpkttbwakaPsGBuKDiYA5atIRqY559W+yUFkMxeOCtzg4PIf4JFlN+1o7VbSJyj3MwP+SImCrPvSGg59PDz2JTeOx6K+aQvH2Cz3A40CHsbf+hgdB04t89/1O/w1cDnyilFU=';
+
+$bot = new BOT_API($channelSecret, $access_token);
+	
+if (!empty($bot->isEvents)) {
+		
+	$bot->replyMessageNew($bot->replyToken, json_encode($bot->message));
+
+	if ($bot->isSuccess()) {
+		echo 'Succeeded!';
+		exit();
+	}
+
+	// Failed
+	echo $bot->response->getHTTPStatus . ' ' . $bot->response->getRawBody(); 
+	exit();
+}
